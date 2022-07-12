de7284e2ad771122d3a12ef1742aa34b63deb9b0
hameshgavali <hameshgavali07@gmail.com>
2022-07-12 19:36:25 +0530

    final updated

--- palindrome_number.py
+++ palindrome_number.py
@@ -1,25 +1,14 @@
-#get the data from user
+condi = True
 
-Number = input("Enter the number to is palindrome or not :")
+while condi:
+    Number = input("Enter the data  to check is  palindrome or not :")
 
-if Number == str(Number)[::-1] :
-    print("This is palindrome number")
-else:
-    print("This is not a palindrome number")
+    if Number == Number[::-1]:
+        print("This is palindrome ")
+    else:
+        print("This is not a palindrome ")
 
-
-choice = input("enter y to continue n to exit ")
-if choice == 'y':
-        print(Number)
-
-print("thanks you for using")
-
-
-
-
-
-
-
-
-
-
+    choice = input("Do you want to continue [y/n] ")
+    if choice == 'n':
+        print("thanks you for using")
+        condi = False
